a581b4b9d215096515fe780a3ad056b6290e5a8c
Jan Lindblad <jlindbla@cisco.com>
2020-03-06 15:35:47 +0100

    Added juniper to evpn service. Fixed protocols/evpn np-container issue in Junos NED. Added latest Arista NED. Added nokia configs for 4.4

--- packages/evpn/python/evpn/main.py
+++ packages/evpn/python/evpn/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- mode: python; python-indent: 4 -*-
 import ncs
 from ncs.application import Service
@@ -33,6 +34,7 @@ class ServiceCallbacks(Service):
             # PREPARE A-SIDE
             vars.add('DEVICE',        root.evpn__evpn.sites.site[service.a].device)
             vars.add('CE_INTERFACE',  root.evpn__evpn.sites.site[service.a].interface)
+            vars.add('RD_IPV4',       root.evpn__evpn.sites.site[service.a].ipv4_address)
 
             # INSTANTIATE A-SIDE
             self.log.info('AAAA apply a-side: ', vars)
@@ -41,6 +43,7 @@ class ServiceCallbacks(Service):
             # PREPARE Z-SIDE
             vars.add('DEVICE',        root.evpn__evpn.sites.site[service.z].device)
             vars.add('CE_INTERFACE',  root.evpn__evpn.sites.site[service.z].interface)
+            vars.add('RD_IPV4',       root.evpn__evpn.sites.site[service.z].ipv4_address)
 
             # INSTANTIATE Z-SIDE
             self.log.info('ZZZZ apply z-side: ', vars)
@@ -56,24 +59,11 @@ class ServiceCallbacks(Service):
 # ---------------------------------------------
 class Main(ncs.application.Application):
     def setup(self):
-        # The application class sets up logging for us. It is accessible
-        # through 'self.log' and is a ncs.log.Log instance.
-        self.log.info('Main RUNNING')
-
-        # Service callbacks require a registration for a 'service point',
-        # as specified in the corresponding data model.
-        #
-        self.register_service('evpn-servicepoint', ServiceCallbacks)
-
-        # If we registered any callback(s) above, the Application class
-        # took care of creating a daemon (related to the service/action point).
-
-        # When this setup method is finished, all registrations are
-        # considered done and the application is 'started'.
+        try:
+            self.log.info('Main RUNNING')
+            self.register_service('evpn-servicepoint', ServiceCallbacks)
+        except Exception as ex:
+            self.log.error('**** Exception: ', ex)
 
     def teardown(self):
-        # When the application is finished (which would happen if NCS went
-        # down, packages were reloaded or some error occurred) this teardown
-        # method will be called.
-
         self.log.info('Main FINISHED')
